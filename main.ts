a2b289273ccc424066bb28ac19adeb075f5e7db7
Arne Van Looveren <arne-vl@hotmail.be>
2024-06-03 15:41:29 +0200

    fixed version format

--- main.ts
+++ main.ts
@@ -14,7 +14,7 @@ if (args.length == 0) {
     repl()
 } else {
     if (args[0] == "-v" || args[0] == "--version") {
-        console.log("KPT version ", VERSION)
+        console.log("KPT version", VERSION)
     } else {
         run(args[0])
     }
@@ -33,7 +33,7 @@ function repl() {
     const parser = new Parser()
     const environment = setup_global_environment()
 
-    console.log("KPT v", VERSION)
+    console.log(`KPT v${VERSION}`)
 
     while (true){
         const input = prompt(">>")
